6295446d109104fd9faf53c61e87a734362e34a1
Aditya-Gaur <75514601+Aditya-Gaur@users.noreply.github.com>
2020-12-06 21:12:38 +0530

    Update SPS-v.2.0.py

--- SPS-v.2.0.py
+++ SPS-v.2.0.py
@@ -18,14 +18,20 @@ engine.runAndWait()
 while True :
     try:
         o = random.randint(0,2)
+        if o == 0:
+            this = 'Rock'
+        elif o == 1:
+            this = 'Paper'
+        else:
+            this = 'Scissor'
         engine.say('Enter your choice')
         engine.runAndWait()
         i = int(input('Enter your choice :\n0 for rock\n1 for paper\n2 for scissor\n:->'))
         if i == o :
-            engine.say('You win')
+            engine.say('Its a tie')
             engine.runAndWait()
-            print('You win\n')
-            W += 1
+            print('Its a tie\n')
+            W += 0
             print(f'Wins = {W}, Loses ={L}\n')
             
         elif i > 2:
@@ -33,23 +39,56 @@ while True :
             engine.runAndWait()
             print('Choose 0, 1 or 2\n')
             print(f'Wins = {W}, Loses ={L}\n')
-        else:
-            engine.say('You loose')
+        elif i == 0:
+            if o == 2:
+                engine.say('You win')
+                engine.runAndWait()
+                print('You win\n')
+                W += 1
+                print(f'Wins = {W}, Loses ={L}\n')
+            else:
+                 engine.say('You loose')
             engine.runAndWait()
             print('You loose')
-            if o == 0:
-                this = 'Rock'
-            elif o == 1:
-                this = 'Paper'
-            else:
-                this = 'Scissor'
             engine.say(f'I chose {this}')
             engine.runAndWait()
             print(f'I chose {this}\n')
             L += 1
             print(f'Wins = {W}, Loses ={L}\n')
             
-
+        elif i == 1:
+            if o == 0:
+                engine.say('You win')
+                engine.runAndWait()
+                print('You win\n')
+                W += 1
+                print(f'Wins = {W}, Loses ={L}\n')
+            else:
+                 engine.say('You loose')
+            engine.runAndWait()
+            print('You loose')
+            engine.say(f'I chose {this}')
+            engine.runAndWait()
+            print(f'I chose {this}\n')
+            L += 1
+            print(f'Wins = {W}, Loses ={L}\n')
+            
+        elif i == 2:
+            if o == 1:
+                engine.say('You win')
+                engine.runAndWait()
+                print('You win\n')
+                W += 1
+                print(f'Wins = {W}, Loses ={L}\n')
+            else:
+                 engine.say('You loose')
+            engine.runAndWait()
+            print('You loose')
+            engine.say(f'I chose {this}')
+            engine.runAndWait()
+            print(f'I chose {this}\n')
+            L += 1
+            print(f'Wins = {W}, Loses ={L}\n')
 
     except:
         engine.say('Invalid Input')
